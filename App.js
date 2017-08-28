53db639f00ac6255cf0770083183a91f63363f26
ZacharyJB <Zachary.jamesbartlett@gmail.com>
2017-08-27 23:57:08 -0400

    basic native will use StackNavigator instead of TabNavigator

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Greetings!',
+  };
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <View>
+    <Text>Welcome to your homepage!</Text>;
+    </View>
     );
   }
 }
+
+const SimpleApp = StackNavigator({
+  Home: {screen: HomeScreen},
+});
+
+AppRegistry.registerComponent('SimpleApp', () => SimpleApp);
 
 const styles = StyleSheet.create({
   container: {
